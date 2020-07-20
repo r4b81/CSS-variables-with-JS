d94361623a27c8dcd825fa131e798ac029de571b
r4b81 <abm.rabbi@gmail.com>
2020-07-20 11:44:29 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  const inputs = document.querySelectorAll(".controls input");
+
+  function handleUpdate() {
+    const px = this.dataset.sizing || ''
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + px)
+  }
+
+  inputs.forEach(input => input.addEventListener("change", handleUpdate))
+  inputs.forEach(input => input.addEventListener("mousemove", handleUpdate))
+}
